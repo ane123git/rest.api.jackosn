cf2b4c117ccf51e9df37d3bac847142ee26b341c
aneesh.bhagat <aneesh.bhagat@accenture.com>
2022-08-16 11:32:39 +0100

    ss

---
+++ src/test/java/com/example/jackson/rest/api/jackosn/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jackson.rest.api.jackosn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
